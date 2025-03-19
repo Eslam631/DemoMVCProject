2f456f67d1b6b0303616a9a70ca97c52e798f167
Eslam631 <aslamterk784@gmail.com>
2025-03-19 13:39:24 +0200

    Add project files.

---
+++ Demo.Perestation/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Demo.Perestation.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
